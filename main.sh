420760de24d82f543ba34cc2140149e829b0a0c2
Liora <liuzii706@gmail.com>
2025-09-24 22:23:57 +0800

    加點東西

---
+++ main.sh
@@ -0,0 +1,6 @@
+python main.py \
+--path_ball_track_model model/ball/model_best.pt \
+--path_court_model model/court/court.pt \
+--path_bounce_model model/Bounce/ctb_regr_bounce.cbm \
+--path_input_video input/tennis4.mp4 \
+--path_output_video output.mp4
